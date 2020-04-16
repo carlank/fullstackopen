1c4489ab2138dd2d6c07b3d98661198b54c9cea0
Nick Sullivan-Friedman <rpgcubed@gmail.com>
2020-04-16 13:55:04 -0700

    uibuild

---
+++ part4/bloglist/backend/build/precache-manifest.19d180b45fc3ee45d7d872ea178a84ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34a21b61b09d31ecf511971312bb9f34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2971f5e9878040293a34",
+    "url": "/static/js/2.f8201993.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8201993.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba58f91f1242c9c0d660",
+    "url": "/static/js/main.47decddf.chunk.js"
+  },
+  {
+    "revision": "76920dbcd809bd5e18e2",
+    "url": "/static/js/runtime-main.af579b3f.js"
+  }
+]);
